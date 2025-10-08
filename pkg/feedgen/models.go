00185a17dea73f8d40731d9bcfab381ee44b06ec
Nick Manos <nick@manos.tech>
2025-10-08 14:38:58 -0400

    Refactor FeedSkeleton to use FeedCursor and enhance cursor parsing functionality

--- pkg/feedgen/models.go
+++ pkg/feedgen/models.go
@@ -1,5 +1,84 @@
 package feedgen
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type CursorType string
+
+const (
+	PostCursor CursorType = "p"
+	RepostCursor CursorType = "r"
+	UnknownCursor CursorType = "u"
+)
+
+type FeedCursor struct {
+	IndexedAt time.Time
+	Type CursorType
+	ID int64
+}
+
+func ParseCursor(cursorStr string) (*FeedCursor, error) {
+	parts := strings.Split(cursorStr, "::")
+
+	if len(parts) < 3 {
+		return nil, fmt.Errorf("invalid cursor format: %s", cursorStr)
+	}
+
+	idxInt, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse IndexedAt in cursor: %w", err)
+	}
+	indexedAt := time.UnixMicro(int64(idxInt))
+
+	cursorType := CursorType(parts[1])
+	switch cursorType {
+	case PostCursor, RepostCursor:
+		break
+	default:
+		cursorType = UnknownCursor
+	}
+
+	id, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse ID in cursor: %w", err)
+	}
+
+	return &FeedCursor{indexedAt, cursorType, int64(id)}, nil
+}
+
+func (fc *FeedCursor) MarshalJSON() ([]byte, error) {
+	idxInt := fc.IndexedAt.UnixMicro()
+	cursorStr := fmt.Sprintf("%d::%s::%d", idxInt, fc.Type, fc.ID)
+
+	return json.Marshal(cursorStr)
+}
+
+func (fc *FeedCursor) UnmarshalJSON(b []byte) error {
+	var cursorStr string
+	if err := json.Unmarshal(b, &cursorStr); err != nil {
+		return err
+	}
+
+	parsed, err := ParseCursor(string(b))
+	if err != nil {
+		return err
+	}
+
+	fc.IndexedAt = parsed.IndexedAt
+	fc.Type = parsed.Type
+	fc.ID = parsed.ID
+
+	return nil
+}
+
 type SkeletonReason string
 
 const (
@@ -18,6 +97,10 @@ type FeedPost struct {
 }
 
 type FeedSkeleton struct {
-	Cursor string `json:"cursor"`
+	Cursor *FeedCursor `json:"cursor"`
 	Feed []FeedPost `json:"feed"`
+}
+
+type FeedBuilder interface {
+	BuildFeed(ctx context.Context, cursor string, limit int, db *bun.DB) *FeedSkeleton
 }
